9a8a4e14d94f1b846bbf424ad7607b5a7da8d816
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-06-29 17:47:18 -0300

    Funcionalidades básicas finalizadas

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import './Feed.css'
 import api from '../services/api'
-
+import io from 'socket.io-client'
 import like from '../assets/like.svg'
 import comment from '../assets/comment.svg'
 import send from '../assets/send.svg'
@@ -12,41 +12,67 @@ class Feed extends Component{
     }
 
     async componentDidMount(){
-        const response = await api.get('posts')
+        this.registerToSocket();
 
+        const response = await api.get('posts')
+        
         this.setState({feed : response.data})
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        // post, like 
+        
+        socket.on('post', newPost =>{
+            this.setState( {feed : [newPost, ...this.state.feed] } )
+        });
+
+        socket.on('like', likedPost =>{
+            this.setState({
+                feed: this.state.feed.map(post =>
+                    post._id === likedPost._id ? likedPost : post
+                )
+            });
+        })
+    }
+
+    handleLike = id => {
+        api.post(`posts/${id}/like`)
     }
 
     render(){
         return (
             <section id='post-list'>
                 {
-                    this.state.feed.map(post =>(
-                        <article>
-                            <header>
-                                <div className='user-info'>
-                                    <span>{post.author}</span>
-                                    <span className='place'>{post.place}</span>
-                                </div>
-                            </header>
-                            <img src={`http://localhost:3333/files/${post.image}`} alt=''/>
-                            
-                            <footer>
-                                <div className='actions'>
+                this.state.feed.map(post =>(
+                    <article key={post._id}>
+                        <header>
+                            <div className='user-info'>
+                                <span>{post.author}</span>
+                                <span className='place'>{post.place}</span>
+                            </div>
+                        </header>
+                        <img src={`http://localhost:3333/files/${post.image}`} alt=''/>
+                        
+                        <footer>
+                            <div className='actions'>
+                                <button onClick={ () => this.handleLike(post._id) }>
                                     <img src={like} alt=''/>
-                                    <img src={comment} alt=''/>
-                                    <img src={send} alt=''/>
-                                </div>
-                                
-                                <strong>{post.likes} curtidas</strong>
+                                </button>
+                                <img src={comment} alt=''/>
+                                <img src={send} alt=''/>
+                            </div>
+                            
+                            <strong>{post.likes} curtidas</strong>
 
-                                <p>
-                                    {post.description}
-                                    <span>{post.hashtags}</span>
-                                </p>
-                            </footer>
-                        </article>
-                    ))
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                ))
                 }
             </section>
         )
